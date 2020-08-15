7ec3bc387bb2b4e741be5dca3b89670b6cf597b6
Josh Nunez <joshjnunez09@gmail.com>
2020-08-14 21:50:21 -0500

    reverting back to og code

--- src/index.js
+++ src/index.js
@@ -24,69 +24,20 @@ class Index extends Component {
 
   grabUserInfo() {
     console.log('button clicked');
-    const apiName = 'jukeapi';
-    const path =
-      'https://y7wzv5d7yc.execute-api.us-east-1.amazonaws.com/dev/grabUsers';
-    const myInit = {
-      // OPTIONAL
-      headers: {}, // OPTIONAL
-      response: true, // OPTIONAL (return the  entire Axios response object instead of only response.data)
-      queryStringParameters: {
-        // OPTIONAL
-        name: 'param',
-      },
-    };
 
-    API.get(apiName, path, myInit)
-      .then((response) => {
-        // Add your code here
-        console.log('HERE IS THE DATA:', response.data);
-      })
-      .catch((error) => {
-        console.log(error.response);
-      });
-
-    // axios
-    //   .get(
-    //     'https://y7wzv5d7yc.execute-api.us-east-1.amazonaws.com/dev/grabUsers'
-    //   )
-    //   .then((res) => console.log('HERE IS YOUR DATA:', res.data))
-    //   .catch((err) => console.error('there was a problem', err));
+    axios
+      .get('https://localhost:3000/grabUsers')
+      .then((res) => console.log('HERE IS YOUR DATA:', res.data))
+      .catch((err) => console.error('there was a problem', err));
   }
 
   addUserInfo() {
     console.log('button clicked');
     //axios call
 
-    const apiName = 'jukeapi';
-    const path =
-      'https://y7wzv5d7yc.execute-api.us-east-1.amazonaws.com/dev/addUsers';
-    const myInit = {
-      // OPTIONAL
-      body: { name: 'Josh' },
-      headers: {}, // OPTIONAL
-      response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
-      queryStringParameters: {
-        // OPTIONAL
-        name: 'param',
-      },
-    };
-
-    API.post(apiName, path, myInit)
-      .then((response) => {
-        // Add your code here
-        console.log('HERE IS THE DATA:', response.data);
-      })
-      .catch((error) => {
-        console.log(error.response);
-      });
-
-    // axios
-    //   .post(
-    //     'https://y7wzv5d7yc.execute-api.us-east-1.amazonaws.com/dev/addUsers',
-    //     { firstName: this.state.name }
-    //   )
-    //   .then(() => console.log('Successful post'));
+    axios
+      .post('https://locahost:3000/addUsers', { firstName: this.state.name })
+      .then(() => console.log('Successful post'));
   }
 
   render() {
